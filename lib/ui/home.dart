4420f010344b861f7c4adeb7edfb348362640470
angeloem <esanga530@gmail.com>
2019-10-11 13:57:28 -0400

    done with the appBar

---
+++ lib/ui/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
+
+class Home extends StatefulWidget{
+	@override
+	State<StatefulWidget> createState() {
+		return new BmiState();
+	}
+}
+
+class BmiState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+		appBar: new AppBar(
+			title: 'Measure BMI',
+			centerTitle: true,
+			backgroundColor: Colors.purple,
+			
+		),
+	);
+  }
+	
+}
